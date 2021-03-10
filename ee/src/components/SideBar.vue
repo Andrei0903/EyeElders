e33c1d4529a994bcbc119bcd5f7769f782dd9dcd
Andrei0903 <krolikakanalturba@mail.ru>
2021-03-10 21:10:03 +0500

    init

---
+++ ee/src/components/SideBar.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-card class="overflow-hidden ">
+    <v-app-bar
+      absolute
+      color="#6A76AB"
+      dark
+      shrink-on-scroll
+      prominent
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-toolbar-title>EyeElders</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-menu
+        bottom
+        left
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            color="yellow"
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab href="/">Профиль</v-tab>
+          <v-tab href="/group">Моя группа</v-tab>
+          <v-tab >Расписания</v-tab>
+          <v-tab>Параличка</v-tab>
+          <v-tab>Чат</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-4"
+      max-height="600"
+    >
+      <v-container style="height: 1000px;"></v-container>
+    </v-sheet>
+  </v-card>
+</template>
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { title: 'Выйти' },
+        { title: 'Настройки' },
+        { title: 'Поиск' },
+        { title: 'Секрет' },
+      ],
+    }),
+  }
+</script>
